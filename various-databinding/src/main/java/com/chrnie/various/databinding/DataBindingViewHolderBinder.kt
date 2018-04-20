7e6d7779e2d5c94eb2f1063d3336dfdb8de68bd2
chrnie <chrnie@qq.com>
2018-04-21 00:46:42 +0800

    Add databinding support

---
+++ various-databinding/src/main/java/com/chrnie/various/databinding/DataBindingViewHolderBinder.kt
@@ -0,0 +1,25 @@
+package com.chrnie.various.databinding
+
+import android.databinding.DataBindingUtil
+import android.databinding.ViewDataBinding
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.chrnie.various.ViewHolderBinder
+
+class DataBindingViewHolderBinder<in T>(private val layoutId: Int, private val variableId: Int)
+    : ViewHolderBinder<T, DataBindingViewHolderBinder.DataBindingViewHolder>() {
+
+
+    override fun onCreateViewHolder(inflater: LayoutInflater, parent: ViewGroup): DataBindingViewHolder {
+        val binding = DataBindingUtil.inflate<ViewDataBinding>(inflater, layoutId, parent, false)
+        return DataBindingViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: DataBindingViewHolder, data: T, payloads: List<Any>?) {
+        holder.binding.setVariable(variableId, data)
+    }
+
+
+    class DataBindingViewHolder(internal val binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root)
+}
