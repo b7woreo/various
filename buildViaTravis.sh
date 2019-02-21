5d536391ccd2c9fd4c8b3b3b28d5299734a4916a
chrnie <chrnie@qq.com>
2019-02-21 12:32:09 +0800

    Fix ci release command

--- buildViaTravis.sh
+++ buildViaTravis.sh
@@ -5,7 +5,7 @@ set -e
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   echo "Build for Release => Branch ["$TRAVIS_BRANCH"]  Tag ["$TRAVIS_TAG"]"
-  ./gradlew -PbintrayUser="$BINTRAY_USER" -PbintrayKey="$BINTRAY_KEY" clean uploadArchives
+  ./gradlew -PbintrayUser="$BINTRAY_USER" -PbintrayKey="$BINTRAY_KEY" clean bintrayUpload
 else
   echo "Build => Branch ["$TRAVIS_BRANCH"]  Tag ["$TRAVIS_TAG"]"
   ./gradlew build
